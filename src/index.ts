a67dd93ec80c12db4890ac15e64400881a6942b7
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2024-04-13 03:55:37 +0200

    loadTest

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,36 @@ import fastify from "fastify";
 
 const server = fastify({ logger: true });
 
+function isPrime(number: number) {
+  if (number <= 1) return false;
+  if (number <= 3) return true;
+
+  if (number % 2 === 0 || number % 3 === 0) return false;
+
+  let i = 5;
+  while (i * i <= number) {
+    if (number % i === 0 || number % (i + 2) === 0) return false;
+    i += 6;
+  }
+
+  return true;
+}
+
+function findPrimesInRange(start: number, end: number) {
+  const primes = [];
+  for (let i = start; i <= end; i++) {
+    if (isPrime(i)) {
+      primes.push(i);
+    }
+  }
+  return primes;
+}
+
 server.get("/ping", async (request, reply) => {
-  return "pongddd\n";
+  const start = 1;
+  const end = 100000;
+  const primes = findPrimesInRange(start, end);
+  return primes;
 });
 
 server.listen({ port: 8080 }, (err, address) => {
