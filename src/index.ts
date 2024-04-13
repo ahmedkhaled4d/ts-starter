dacff725312037da6ad16a70b6df0ecfd9e835e0
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2024-04-13 03:39:03 +0200

    nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import fastify from "fastify";
 
-const server = fastify();
+const server = fastify({ logger: true });
 
 server.get("/ping", async (request, reply) => {
-  return "pong\n";
+  return "pongddd\n";
 });
 
 server.listen({ port: 8080 }, (err, address) => {
